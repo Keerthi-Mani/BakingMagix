26d9c95d1e20090beb3c1f8737f71d46558c8837
Keerthi Mani <carkeane.getz@gmail.com>
2020-06-11 14:29:09 -0400

    gallery nav

--- js/materialize.js
+++ js/materialize.js
@@ -3,12 +3,17 @@ const sideNav = document.querySelector(".sidenav");
 //Materialize js library - use M
 M.Sidenav.init(sideNav, {});
 
+//Side Gallery Menu
+const galleryNav = document.querySelector(".gallerynav");
+//Materialize js library - use M
+M.Sidenav.init(galleryNav, {});
+
 //Slider
 const slider = document.querySelector(".slider");
 M.Slider.init(slider, {
   indicators: false,
   height: 500,
-  interval: 6000
+  interval: 6000,
 });
 
 // Scrollspy
